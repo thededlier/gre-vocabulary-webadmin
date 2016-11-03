e201246d9ca5b8ee3ff92a39884bb3327bcebc0f
Rohan Anand <thededlier@gmail.com>
2016-11-03 22:44:51 +0530

    Added firebase dbv

---
+++ js/firebasedb.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD81iwuCfGQVC8_rhwNYRaJBadTfuZNDyI",
+    authDomain: "gre-vocabulary-app.firebaseapp.com",
+    databaseURL: "https://gre-vocabulary-app.firebaseio.com",
+    storageBucket: "gre-vocabulary-app.appspot.com",
+    messagingSenderId: "976263507443"
+};
+
+firebase.initializeApp(config);
