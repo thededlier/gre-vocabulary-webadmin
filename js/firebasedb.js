eabe7c206172f38718ecadab3d08745ddc4145f8
Rohan Anand <thededlier@gmail.com>
2016-11-06 19:12:50 +0530

    implemented search function

--- js/firebasedb.js
+++ js/firebasedb.js
@@ -47,16 +47,29 @@ function newWordAdd(word, definition) {
     document.body.insertBefore(div, document.getElementById('end'));
 }
 
+function searchWord(word) {
+    var wordListRef = firebase.database().ref('/');
+    var wordKey;
+
+    wordListRef.child('wordlist').orderByChild('word').equalTo(word).on('child_added', function(data) {
+        wordKey = data.key;
+    });
+
+    return wordKey;
+
+}
+
 // On page load
 window.addEventListener('load', function() {
     console.log('Loaded');
 
     var wordListRef =   firebase.database().ref('/wordlist/');
-    var addWordForm =   document.getElementById("addWord-form");
+    var addWordForm =   document.getElementById('addWord-form');
+    var updateWordForm  =   document.getElementById('updateWord-form');
+    var deleteWordForm  =   document.getElementById('deleteWord-form');
 
     // Listen for changes and update
     wordListRef.on('child_added', function(data) {
-        console.log(data);
         newWordAdd(data.val().word, data.val().definition);
     });
 
@@ -71,7 +84,7 @@ window.addEventListener('load', function() {
     // Write word to wordlist
     addWordForm.onsubmit = function(e) {
 
-        console.log('Submitted new word');
+        console.log('Attempting to submit new word');
         var word    = document.getElementById('new_word').value;
         var wordDef = document.getElementById('word_def').value;
 
@@ -83,4 +96,21 @@ window.addEventListener('load', function() {
             document.getElementById('word_def').value   = '';
         }
     };
+
+    updateWordForm.onsubmit = function(e) {
+
+        console.log('Attempting to update word');
+        var word    =   document.getElementById('update-word');
+        var wordDef =   document.getElementById('update-word-def');
+        
+        if (word != '' && wordDef != '') {
+            
+            var key = searchWord(word.value);
+            console.log('Key : ' + key);
+            console.log('Pushed word changes');
+
+            document.getElementById('update-word').value   = '';
+            document.getElementById('update-word-def').value   = '';
+        }
+    }
 }, false);
